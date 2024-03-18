acd7e6be3a21477842c5d70cbc547b129f6e5820
irisreis <irisreis1408@gmail.com>
2024-03-18 20:54:16 -0300

    homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { HeadlineComponent } from '../../components/headline/headline.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CombosComponent } from '../../components/combos/combos.component';
+import { InicioComponent } from '../../components/inicio/inicio.component';
+import { ApresentacaoComponent } from '../../components/apresentacao/apresentacao.component';
+import { BeneficiosComponent } from '../../components/beneficios/beneficios.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeadlineComponent, HeaderComponent],
+  imports: [ HeaderComponent, CombosComponent, InicioComponent, ApresentacaoComponent, BeneficiosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
