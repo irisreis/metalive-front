77dfe2815e9745c93420c9e0b601a831801c3bca
Iris <irisreis1408@gmail.com>
2024-04-16 20:01:16 -0300

    pagina de perfil quase completa

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { ProfissionalComponent } from '../../components/profissional/profissional.component';
+import { DadosComponent } from '../../components/dados/dados.component';
+import { RodapeComponent } from '../../components/rodape/rodape.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [ProfissionalComponent],
+  imports: [HeaderComponent, ProfissionalComponent, DadosComponent, RodapeComponent],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.scss'
 })
