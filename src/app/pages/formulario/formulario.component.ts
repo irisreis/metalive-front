35e5470fd4956e71b29e0937d11f15ae8769e9c9
irisreis <irisreis1408@gmail.com>
2024-06-27 23:17:30 -0300

    pagina formulario

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { RodapeComponent } from '../../components/rodape/rodape.component';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RodapeComponent],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss'
 })
