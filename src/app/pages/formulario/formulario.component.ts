9db1906d5f0a47bf4c4a7d5ba2f80f336bbe33ad
irisreis <irisreis1408@gmail.com>
2024-06-29 14:08:18 -0300

    comeco da estolizacao da pagina do fprmulario

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../components/header/header.component';
+import { Header2Component } from '../../components/header2/header2.component';
 import { RodapeComponent } from '../../components/rodape/rodape.component';
 import { FormComponent } from '../../components/form/form.component';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [HeaderComponent, RodapeComponent, FormComponent],
+  imports: [Header2Component, RodapeComponent, FormComponent],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss'
 })
