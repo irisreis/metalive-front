73e8c7e333e5a9c26ced49e2165afe7e96cbf3e0
Gustavo Kropiwiec <gustavosk17@hotmail.com>
2024-08-04 19:41:44 -0300

    login e guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { ComboComponent } from './pages/combo/combo.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { FormularioComponent } from './pages/formulario/formulario.component';
+import { userGuard } from './guards/user.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'cadastro', component: CadastroComponent },
     { path: 'combos', component: ComboComponent },
-    { path: 'perfil', component: PerfilComponent },
+    { path: 'perfil', component: PerfilComponent, canActivate: [userGuard] },
     { path: 'formulario', component: FormularioComponent},
 ];
 
