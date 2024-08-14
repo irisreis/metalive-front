b85357e8e7d9ef9c01a8e1e80f05823c6b4f31a4
irisreis <irisreis1408@gmail.com>
2024-08-13 23:46:17 -0300

    header

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Header2Component } from '../../components/header2/header2.component';
+import { HeaderComponent } from '../../components/header/header.component';
 import { RodapeComponent } from '../../components/rodape/rodape.component';
 import { FormComponent } from '../../components/form/form.component';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [Header2Component, RodapeComponent, FormComponent],
+  imports: [HeaderComponent, RodapeComponent, FormComponent],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss'
 })
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { ProfissionalComponent } from '../../components/profissional/profissional.component';
 import { DadosComponent } from '../../components/dados/dados.component';
 import { RodapeComponent } from '../../components/rodape/rodape.component';
-import { Header2Component } from '../../components/header2/header2.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [Header2Component, ProfissionalComponent, DadosComponent, RodapeComponent],
+  imports: [HeaderComponent, ProfissionalComponent, DadosComponent, RodapeComponent],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.scss'
 })
