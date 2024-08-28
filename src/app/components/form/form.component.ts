fa2684181a01753f5d2f8794f65e4ece83e149db
irisreis <irisreis1408@gmail.com>
2024-08-28 00:16:18 -0300

    js do checkbox do formulario e pag de dados pgto

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
-
+  onlyOne(eventTarget: EventTarget | null, groupName: string): void {
+    const checkbox = eventTarget as HTMLInputElement;
+    if (!checkbox) {
+      console.error('Checkbox não foi encontrado.');
+      return;
+    }
+    const checkboxes = document.getElementsByName(groupName) as NodeListOf<HTMLInputElement>;
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) {
+            item.checked = false;
+        }
+    });
+  }
 }
